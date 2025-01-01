cd7ad084fad6efa506e30ed17eb463275a1310db
Zip <thantzinhtet2001@gmail.com>
2025-01-01 21:05:20 +0630

    env update

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,9 +1,9 @@
+// export const BASE_URL = "http://backend.local/api";
 export const BASE_URL = "http://127.0.0.1:8000/api";
-export const API_URL = `${BASE_URL}/v1`;
 
 const config = {
-  API_URL: "http://127.0.0.1:8000/api/v1",
-  Img_URL: "http://127.0.0.1:8000/api/v1",
+  API_URL: `${BASE_URL}/v1`,
+  Img_URL: `${BASE_URL}/v1`,
   // Placeholder_Img: "/assets/placeholder.png",
 };
 
